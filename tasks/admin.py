68b21d5383e277485f9a30028f61ebb131734163
Hilal <hilal.sahin@kcl.ac.uk>
2023-11-01 11:03:06 +0000

    create list_display in admin.py, create superadmin with @admin , 1234 password

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for Users. """
+
+    list_display = ['username', 'first_name', 'last_name', 'email', 'is_active']
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
